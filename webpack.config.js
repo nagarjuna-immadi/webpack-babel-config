9cbbf43db854815993dd17c8dd7732501e29cfa8
Nagarjun <w3.nagarjuna@gmail.com>
2022-04-22 11:32:45 +0530

    webpack babel setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    liveReload: true,
+    open: true,
+    hot: true
+  },
+  entry: {
+    "arrow-functions": "./src/arrow-functions.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/code"),
+    filename: "output.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+};
